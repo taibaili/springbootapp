fdf6526a56c60d029637f0a4a28c571737f15b22
Yang X <demo.xy2008@gmail.com>
2017-07-14 15:25:34 -0400

    second commit from local spring boot app to my github

---
+++ src/main/java/com/example/domain/Request.java
@@ -0,0 +1,7 @@
+package com.example.domain;
+
+/**
+ * Created by Yang on 7/13/2017.
+ */
+public class Request {
+}
---
+++ src/main/java/com/example/service/UserService.java
@@ -0,0 +1,7 @@
+package com.example.service;
+
+/**
+ * Created by Yang on 7/13/2017.
+ */
+public class UserService {
+}
